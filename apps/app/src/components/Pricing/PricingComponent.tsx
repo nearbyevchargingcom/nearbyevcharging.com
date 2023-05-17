3f9b82ea70f8bdcb4a3af6c67c4660a79dcdffa5
Sonu Kumar <skbhati199@gmail.com>
2023-05-17 11:01:10 +0530

    add gradient in pricing page

--- apps/app/src/components/Pricing/PricingComponent.tsx
+++ apps/app/src/components/Pricing/PricingComponent.tsx
@@ -10,7 +10,7 @@ const pricingCards: PricingCardModel[] = [
     description: "Best option for personal use & for your next project.",
     pricePerMonth: "29",
     pricePerYearly: "299",
-    buttonName : "Buy",
+    buttonName: "Buy",
     features: [
       {
         id: "0",
@@ -40,7 +40,7 @@ const pricingCards: PricingCardModel[] = [
     description: "Relevant for multiple users, extended & premium support.",
     pricePerMonth: "99",
     pricePerYearly: "999",
-    buttonName : "Start a free trial",
+    buttonName: "Start a free trial",
     features: [
       {
         id: "0",
@@ -67,12 +67,12 @@ const pricingCards: PricingCardModel[] = [
   {
     id: "Enterprise",
     plan: "Enterprise",
-    isBusiness:true,
+    isBusiness: true,
     description:
       "Best for large scale uses and extended redistribution rights.",
     pricePerMonth: "499",
     pricePerYearly: "4999",
-    buttonName : "Contact Sales",
+    buttonName: "Contact Sales",
     features: [
       {
         id: "0",
@@ -101,8 +101,20 @@ const pricingCards: PricingCardModel[] = [
 const PricingComponent = () => {
   return (
     <>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+      <section className="relative isolate bg-white px-6 pt-14 dark:bg-gray-900 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-screen-xl  py-8 px-4 lg:py-16 lg:px-6">
           <div className="mx-auto mb-8 max-w-screen-md text-center  lg:my-20">
             <h2 className="mb-4 text-6xl font-extrabold tracking-tight text-gray-900 dark:text-white">
               Affordable Rates for Exceptional Service
